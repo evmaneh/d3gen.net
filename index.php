47f8af7f70e6e90c858382d8f78defc2752601ce
evelyn <122318862+evmaneh@users.noreply.github.com>
2025-02-11 13:25:07 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include 'meta.php'; ?>
+<link rel="stylesheet" href="indexstyles.css">
+
+    <title>D3gen.NetWork</title>
+</head>
+<body>
+    <div class="title">
+        <img src="public/images/degen.network.png">
+        <div class="clipping-container">
+            <div class="clipping-box"></div>
+            <div class="clipping-box"></div>
+        </div>
+        <div class="links">
+            <a href="/pages">Next</a>
+            <a href="/hacked">Email hacked (info)</a>
+            <a href="https://discord.d3gen.net" target="_blank">New server</a>
+            <a href="/about-me">Profile</a>
+            <a href="https://discord.d3gen.net/boosters/login.php">Boosters</a>
+            <a href="https://chat.d3gen.net/">Chat</a>
+        </div>
+    </div>
+    <div class="emoji-container">
+        <div class="emoji"> >:) </div>
+        <div class="emoji-second"> 凸 </div>
+    </div>
+</body>
+</html>
